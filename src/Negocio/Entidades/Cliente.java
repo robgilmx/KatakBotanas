1b97a25267c60769bb8420c83315ecfcd6d19053
khyon <mayitosonrics@hotmail.com>
2016-10-24 23:06:14 -0500

    Update Cliente.java

--- src/Negocio/Entidades/Cliente.java
+++ src/Negocio/Entidades/Cliente.java
@@ -4,39 +4,21 @@
  * and open the template in the editor.
  */
 package Negocio.Entidades;
-   
-/**
- *
- * @author DEMON
- */
+
 public class Cliente {
     private int id;
     private String nombre;
     private String telefono;
-    private String celular;
     private String direccion;
 
-    public Cliente(){
-        
-    }
-    
-    public Cliente(String nombre,
-             String telefono,
-             String direccion
+ 
+    public Cliente(int id, 
+                   String nombre,
+                   String telefono,
+                   String direccion
     )  {
         this.nombre = nombre;
         this.telefono = telefono;
-        this.direccion = direccion;
-    }
-    
-    public Cliente(String nombre,
-             String telefono,
-             String celular,
-             String direccion
-    )  {
-        this.nombre = nombre;
-        this.telefono = telefono;
-        this.celular = celular;
         this.direccion = direccion;
     }
     
@@ -62,14 +44,6 @@ public class Cliente {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
     }
 
     public String getDireccion() {
